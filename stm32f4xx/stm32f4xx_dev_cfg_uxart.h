bf8b5bf35dc362e9d4e33471235e51c0595566cf
Rafal Wojcik <wrs.code@gmail.com>
2018-01-14 15:09:00 +0100

    Added UXART base config and rcc function

---
+++ stm32f4xx/stm32f4xx_dev_cfg_uxart.h
@@ -0,0 +1,49 @@
+/*
+ *  Created on: Dec 17, 2017
+ *  Author:     wrscode
+ *  File:       stm32f4xx_uxart_cfg.h
+ */
+
+#ifndef STM32F4XX_DEV_CFG_UXART_H_
+#define STM32F4XX_DEV_CFG_UXART_H_
+
+#include "stm32f4xx.h"
+
+namespace Stm32f4xx
+{
+namespace Device
+{
+namespace Config
+{
+namespace Uxart
+{
+template<size_t UxartNumber>
+struct Structure {
+    enum
+    {
+        BaseAddress = 0U, //
+        IrqNumber = 0U, //
+        ClockEnableRegAddress = 0U, //
+        ClockEnableBitNumber = 0U, //
+        PClkNumber = 0U, //
+    };
+};
+
+template<>
+struct Structure<1>
+{
+    enum
+    {
+        BaseAddress = USART1_BASE,//
+        IrqNumber = USART1_IRQn, //
+        ClockEnableRegAddress = RCC_BASE + 0x44U,//
+        ClockEnableBitNumber = 4U,//
+        PClkNumber = 2, //
+    };
+};
+} // namespace Uxart
+} // namespace Config
+} // namespace Device
+} // namespace Stm32f4xx
+
+#endif /* STM32F4XX_DEV_CFG_UXART_H_ */
--- stm32f4xx/stm32f4xx_rcc.h
+++ stm32f4xx/stm32f4xx_rcc.h
@@ -7,8 +7,9 @@
 #ifndef STM32F4XX_RCC_H_
 #define STM32F4XX_RCC_H_
 
-#include <stm32f4xx_bitband.h>
-#include <stm32f4xx_dev_cfg_gpio.h>
+#include "stm32f4xx_bitband.h"
+#include "stm32f4xx_dev_cfg_gpio.h"
+#include "stm32f4xx_dev_cfg_uxart.h"
 #include "stm32_sys.h"
 
 namespace Stm32f4xx
@@ -17,9 +18,12 @@ namespace Device
 {
 namespace Rcc
 {
-template<char Letter> /* GPIO */
+/* GPIO ************************************************************************************************************ */
+template<char Letter>
 struct Gpio
 {
+    static_assert(Device::Config::Gpio::Structure<Letter>::ClockEnableRegAddress != 0U, "GPIO clock config wrong !!!");
+
 private:
     enum
     {
@@ -40,6 +44,33 @@ public:
 template<char Letter>
 const Stm32::System::Utils::ConversionAddressToPointerType< //
         Gpio<Letter>::ClkEnBBAddr, volatile std::uint32_t> Gpio<Letter>::mClockEnable;
+
+/* UXART *********************************************************************************************************** */
+template<size_t Number>
+struct Uxart
+{
+    static_assert(Device::Config::Uxart::Structure<Number>::ClockEnableRegAddress != 0U, "UXART clock config wrong !!!");
+
+private:
+    enum
+    {
+        ClkEnBBAddr = Stm32f4xx::BitBand::BitInRegPeriphAddr2BBAddr< //
+                Device::Config::Uxart::Structure<Number>::ClockEnableRegAddress, //
+                Device::Config::Uxart::Structure<Number>::ClockEnableBitNumber>::BBAddress
+    };
+
+    static const Stm32::System::Utils::ConversionAddressToPointerType<ClkEnBBAddr, volatile std::uint32_t> mClockEnable;
+
+public:
+    static void setClock(const Stm32::System::State& NewState)
+    {
+        *mClockEnable = (Stm32::System::State::Enable == NewState) ? 0x01U : 0x00U;
+    }
+};
+
+template<size_t Number>
+const Stm32::System::Utils::ConversionAddressToPointerType< //
+        Uxart<Number>::ClkEnBBAddr, volatile std::uint32_t> Uxart<Number>::mClockEnable;
 
 } // namespace Rcc
 } // namespace Device
